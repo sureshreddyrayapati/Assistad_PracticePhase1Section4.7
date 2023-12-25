1a8eb8c4a047b8a9d15a6b2516cdc72f05681476
sureshreddyrayapati <resuresh0@gmail.com>
2023-12-25 16:51:19 +0530

    Add project files.

---
+++ Phase1Section4.7b/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phase1Section4._7b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            runApp();
+        }
+        public static void runApp()
+        {
+            SingleLinkedList list = new SingleLinkedList();
+            Node n = new Node();
+            n.Data = "root";
+            list.add(n);
+            for (int i = 0; i < 10; i++)
+            {
+                Node node = new Node();
+                node.Data = Convert.ToString(i);
+                list.add(node);
+            }
+
+            Console.WriteLine("length of singly linked list=" + list.getLengthOfList());
+
+            n = list.getRoot();
+            while (n != null)
+            {
+                Console.WriteLine(n.Data);
+                n = list.getNext();
+            }
+        }
+    }
+}
